89a7c0435672645223c846719430b1b60ddf642e
Alireza <alirezasakhaeirad@gmail.com>
2023-07-16 13:03:53 +0200

    studid changes. back to work

--- NLP.py
+++ NLP.py
@@ -1,4 +1,23 @@
+from datasets import load_dataset
+import datasets
+import numpy as np
 ######################################################
 ########### Dataset Fixer For CLassification ##########
 ######################################################
 
+# def fit_mcq_into_template(dataset = datasets.dataset_dict.DatasetDict()):
+#     dataset = dataset.rename_column('answer', 'target')
+#
+#     new_column = ["foo"] * len(dataset)
+#     dataset = dataset.add_column("new_column", new_column)
+#     temp = np.zeros(len(dataset))
+#     dataset['optA'] = temp
+#
+#     return dataset
+#
+#
+#
+# dataset = load_dataset('tasksource/mmlu', 'abstract_algebra')
+# dataset = fit_mcq_into_template(dataset)
+# print(type(dataset))
+# print(dataset['test'])
